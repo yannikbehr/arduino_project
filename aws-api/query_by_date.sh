ba703ace9729b62c3a0fd646b31c3d883a0de871
Jonas Behr <jonas_behr@web.de>
2022-10-20 13:39:02 +0200

    adjust script to new sort key

--- aws-api/query_by_date.sh
+++ aws-api/query_by_date.sh
@@ -10,5 +10,11 @@ fi
 
 source DyDB/DyDB_object.sh
 
-DyDB_query MyBeeDataTable WeightSort_$date --partition WeightKg --raw-output | jq -r '.Items[] | ["WeightKg", .MyValue.S, .mySortKey.S ] | @tsv'
-DyDB_query MyBeeDataTable WeightSort_$date --partition TempC --raw-output | jq -r '.Items[] | ["TempC", .MyValue.S, .mySortKey.S ] | @tsv'
+for partition in WeightKg TempC Volt; do
+    #DyDB_query MyBeeDataTable SortKey_$date --partition "$partition" --raw-output | jq --arg partition "$partition" -r '.Items[] | ["partition", .MyValue.S, .mySortKey.S ] | @tsv'
+    DyDB_query MyBeeDataTable SortKey_$date --partition "$partition" --raw-output | jq -r --arg partition "$partition" '.Items[] | [$partition, .MyValue.S, .mySortKey.S ] | @tsv'
+done
+
+
+#DyDB_query MyBeeDataTable SortKey_$date --partition TempC --raw-output | jq -r '.Items[] | ["TempC", .MyValue.S, .mySortKey.S ] | @tsv'
+#DyDB_query MyBeeDataTable SortKey_$date --partition Volt --raw-output | jq -r '.Items[] | ["Volt", .MyValue.S, .mySortKey.S ] | @tsv'
